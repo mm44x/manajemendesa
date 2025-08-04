7774f9bf9a9e72e387ee5ca0a6f5e1edd26adca6
mm44x <maxtkj@outlook.com>
2025-08-05 01:54:33 +0700

    Penyempurnaan Middleware

--- routes/web.php
+++ routes/web.php
@@ -8,19 +8,6 @@ use App\Http\Controllers\WilayahController;
 use App\Http\Controllers\WargaController;
 use App\Http\Controllers\WargaExportController;
 
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -29,32 +16,41 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth'])->group(function () {
+
+    // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::middleware(['auth', 'role:admin'])->group(function () {
-        // Route::get('/users', [UserController::class, 'index']);
-    });
 
-    Route::middleware(['auth', 'role:admin,sekretaris'])->group(function () {
-        Route::resource('kartu-keluarga', KartuKeluargaController::class);
-    });
-
+    // Master Wilayah (akses umum, AJAX)
     Route::get('/wilayah/provinsi', [WilayahController::class, 'getProvinsi']);
     Route::get('/wilayah/kabupaten', [WilayahController::class, 'getKabupaten']);
     Route::get('/wilayah/kecamatan', [WilayahController::class, 'getKecamatan']);
     Route::get('/wilayah/desa', [WilayahController::class, 'getDesa']);
 
-    Route::get('/kartu-keluarga/{id}/anggota', [AnggotaKeluargaController::class, 'indexByKK'])->name('anggota-keluarga.index');
-    Route::post('/anggota-keluarga', [AnggotaKeluargaController::class, 'store'])->name('anggota-keluarga.store');
-    Route::put('/anggota-keluarga/{id}', [AnggotaKeluargaController::class, 'update'])->name('anggota-keluarga.update');
-    Route::delete('/anggota-keluarga/{id}', [AnggotaKeluargaController::class, 'destroy'])->name('anggota-keluarga.destroy');
-
-    Route::middleware(['auth'])->group(function () {
-        Route::get('/semua-warga', [WargaController::class, 'index'])->name('semua-warga.index');
+    // 📁 Modul Kartu Keluarga - Hanya Admin dan Sekretaris
+    Route::middleware(['role:admin,sekretaris'])->group(function () {
+        Route::resource('kartu-keluarga', KartuKeluargaController::class);
     });
-    Route::get('/warga/export', [WargaExportController::class, 'export'])->name('warga.export');
+
+    // 👨‍👩‍👧 Modul Anggota Keluarga - Hanya Sekretaris
+    Route::middleware(['role:sekretaris'])->group(function () {
+        Route::post('/anggota-keluarga', [AnggotaKeluargaController::class, 'store'])->name('anggota-keluarga.store');
+        Route::put('/anggota-keluarga/{id}', [AnggotaKeluargaController::class, 'update'])->name('anggota-keluarga.update');
+        Route::delete('/anggota-keluarga/{id}', [AnggotaKeluargaController::class, 'destroy'])->name('anggota-keluarga.destroy');
+    });
+
+    // 👁️ Lihat Anggota Keluarga per KK - Semua Role
+    Route::get('/kartu-keluarga/{id}/anggota', [AnggotaKeluargaController::class, 'indexByKK'])->name('anggota-keluarga.index');
+
+    // 📄 Lihat Semua Warga - Semua Role
+    Route::get('/semua-warga', [WargaController::class, 'index'])->name('semua-warga.index');
+
+    // 📤 Export Excel - Hanya Admin dan Sekretaris
+    Route::middleware(['role:admin,sekretaris'])->group(function () {
+        Route::get('/warga/export', [WargaExportController::class, 'export'])->name('warga.export');
+    });
 });
 
 require __DIR__ . '/auth.php';
